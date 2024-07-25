329be5add5432088b3d6eec509efe675bb3df024
jay-itl <weijingjaylin@gmail.com>
2024-07-25 15:24:23 -0700

    test the udpate?

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = defineConfig({
-  transpileDependencies: true,
-  configureWebpack: {
-    plugins: [
-      new HtmlWebpackPlugin({
-        title: "Vue Todo App", // Set the title here
-        template: "public/index.html", // Path to the template file
-        filename: "index.html",
-        inject: true,
-      }),
-    ],
-  },
+  transpileDependencies: true
 });
